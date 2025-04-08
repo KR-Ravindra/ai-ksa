1d8a1bb263d02ddb08265c34d63fc6d5488474ca
Raja Ravindra Kathi <kr-ravindra@Rajas-Air.lan>
2025-04-08 11:47:41 -0700

    Added workflow to build images

--- operator/internal/controller/aihorizontalpodautoscaler_controller.go
+++ operator/internal/controller/aihorizontalpodautoscaler_controller.go
@@ -1,23 +1,8 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
+	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -27,7 +12,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/utils/pointer"
+	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -36,49 +21,47 @@ import (
 	autoscalingv1 "github.com/kr-ravindra/ai-ksa/api/v1"
 )
 
+var (
+	k8sClientSet     *kubernetes.Clientset
+	metricsClientSet *metricsv.Clientset
+)
+
+// Initialize Kubernetes and Metrics clients
+func init() {
+	logger := log.FromContext(context.Background())
+	config, err := rest.InClusterConfig() // Works when running inside the cluster
+	if err != nil {
+		logger.Error(err, "Failed to get in-cluster config, trying to load kubeconfig")
+		if err != nil {
+			panic(err)
+		}
+	}
+	k8sClientSet, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	metricsClientSet, err = metricsv.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+}
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;update
+// +kubebuilder:rbac:groups=autoscaling.cortex.me,resources=aihorizontalpodautoscalers,verbs=get;list;watch
+// +kubebuilder:rbac:groups=autoscaling.cortex.me,resources=aihorizontalpodautoscalers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=autoscaling.cortex.me,resources=aihorizontalpodautoscalers/finalizers,verbs=update
+
 // AIHorizontalPodAutoscalerReconciler reconciles a AIHorizontalPodAutoscaler object
 type AIHorizontalPodAutoscalerReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=autoscaling.cortex.me,resources=aihorizontalpodautoscalers,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=autoscaling.cortex.me,resources=aihorizontalpodautoscalers/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=autoscaling.cortex.me,resources=aihorizontalpodautoscalers/finalizers,verbs=update
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the AIHorizontalPodAutoscaler object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
-
-var k8sClientSet *kubernetes.Clientset
-
-func init() {
-    config, err := rest.InClusterConfig() // Works when running inside the cluster
-    if err != nil {
-        kubeconfigPath := "/path/to/your/kubeconfig" // Replace with your kubeconfig path
-        config, err = config.BuildConfigFromFlags("", kubeconfigPath) // Works for local testing
-        if err != nil {
-            panic(err)
-        }
-    }
-    k8sClientSet, err = kubernetes.NewForConfig(config)
-    if err != nil {
-        panic(err)
-    }
-}
-
-
+// Reconcile function
 func (r *AIHorizontalPodAutoscalerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
-	/// 1. Fetch the AIHorizontalPodAutoscaler instance
+	// 1. Fetch the AIHorizontalPodAutoscaler instance
 	aihpa := &autoscalingv1.AIHorizontalPodAutoscaler{}
 	err := r.Get(ctx, req.NamespacedName, aihpa)
 	if err != nil {
@@ -90,58 +73,67 @@ func (r *AIHorizontalPodAutoscalerReconciler) Reconcile(ctx context.Context, req
 		return ctrl.Result{}, err
 	}
 
-	// 2. Define the "hello world" Deployment
-	logger.Info("Reconciling AIHorizontalPodAutoscaler", "AIHorizontalPodAutoscaler.Namespace", aihpa.Namespace, "AIHorizontalPodAutoscaler.Name", aihpa.Name)
-	helloWorldDeployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "hello-world-" + aihpa.Name,
-			Namespace: aihpa.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: pointer.Int32(1),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"app": "hello-world-" + aihpa.Name},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{"app": "hello-world-" + aihpa.Name},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:            "hello-world",
-							Image:           "busybox", // Use a simple image
-							Command:         []string{"/bin/sh", "-c", "echo 'Hello from my Operator!' && sleep 3600"},
-							ImagePullPolicy: corev1.PullIfNotPresent,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	// Set the AIHorizontalPodAutoscaler instance as the owner and controller of the Deployment
-	ctrl.SetControllerReference(aihpa, helloWorldDeployment, r.Scheme)
-
-	// 3. Check if the Deployment exists and create it if it doesn't
-	found := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: helloWorldDeployment.Name, Namespace: helloWorldDeployment.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		logger.Info("Creating a new Deployment", "Deployment.Namespace", helloWorldDeployment.Namespace, "Deployment.Name", helloWorldDeployment.Name)
-		err = r.Create(ctx, helloWorldDeployment)
-		if err != nil {
-			logger.Error(err, "Failed to create Deployment", "Deployment.Namespace", helloWorldDeployment.Namespace, "Deployment.Name", helloWorldDeployment.Name)
-			return ctrl.Result{}, err
+	// 2. Fetch the target deployment
+	targetDeployment := &appsv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Name: aihpa.Spec.TargetDeploymentName, Namespace: aihpa.Spec.TargetDeploymentNamespace}, targetDeployment)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Error(err, "Target deployment not found", "Namespace", aihpa.Spec.TargetDeploymentNamespace, "Name", aihpa.Spec.TargetDeploymentName)
+			return ctrl.Result{}, nil
 		}
-		return ctrl.Result{Requeue: true}, nil // Requeue to ensure status is updated
-	} else if err != nil {
-		logger.Error(err, "Failed to get Deployment", "Deployment.Namespace", helloWorldDeployment.Namespace, "Deployment.Name", helloWorldDeployment.Name)
+		logger.Error(err, "Failed to get target deployment")
 		return ctrl.Result{}, err
 	}
 
-	// 4. (We're NOT doing any scaling logic in this simplified version)
+	// 3. Query the Metrics Server for CPU usage
+	podList := &corev1.PodList{}
+	err = r.List(ctx, podList, client.InNamespace(aihpa.Spec.TargetDeploymentNamespace), client.MatchingLabels(targetDeployment.Spec.Selector.MatchLabels))
+	if err != nil {
+		logger.Error(err, "Failed to list pods for target deployment")
+		return ctrl.Result{}, err
+	}
 
-	return ctrl.Result{}, nil
+	totalCPUUsage := int64(0)
+	for _, pod := range podList.Items {
+		metrics, err := metricsClientSet.MetricsV1beta1().PodMetricses(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
+		if err != nil {
+			logger.Error(err, "Failed to get metrics for pod", "Pod.Name", pod.Name)
+			continue
+		}
+		for _, container := range metrics.Containers {
+			cpuUsage, _ := container.Usage.Cpu().AsInt64()
+			totalCPUUsage += cpuUsage
+		}
+	}
+
+	// Calculate average CPU usage
+	averageCPUUsage := totalCPUUsage / int64(len(podList.Items))
+	logger.Info("CPU Usage", "AverageCPUUsage", averageCPUUsage, "Threshold", aihpa.Spec.TargetDeploymentCPUThreshold)
+
+	// 4. Scale the deployment if CPU usage exceeds the threshold
+	currentReplicas := *targetDeployment.Spec.Replicas
+	if averageCPUUsage > int64(aihpa.Spec.TargetDeploymentCPUThreshold) && currentReplicas < int32(aihpa.Spec.MaxReplicas) {
+		newReplicas := currentReplicas + 1
+		targetDeployment.Spec.Replicas = &newReplicas
+		err = r.Update(ctx, targetDeployment)
+		if err != nil {
+			logger.Error(err, "Failed to scale up target deployment")
+			return ctrl.Result{}, err
+		}
+		logger.Info("Scaled up target deployment", "NewReplicas", newReplicas)
+	} else if averageCPUUsage < int64(aihpa.Spec.TargetDeploymentCPUThreshold) && currentReplicas > int32(aihpa.Spec.MinReplicas) {
+		newReplicas := currentReplicas - 1
+		targetDeployment.Spec.Replicas = &newReplicas
+		err = r.Update(ctx, targetDeployment)
+		if err != nil {
+			logger.Error(err, "Failed to scale down target deployment")
+			return ctrl.Result{}, err
+		}
+		logger.Info("Scaled down target deployment", "NewReplicas", newReplicas)
+	}
+
+	// 5. Requeue periodically to monitor CPU usage
+	return ctrl.Result{RequeueAfter: time.Second}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
